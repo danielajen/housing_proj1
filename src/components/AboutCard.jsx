51e7502bd659cfbf02035f264f5bcc0e6a7a616d
Daniel Ajenifuja <dajenifu@uwo.ca>
2025-05-08 11:17:02 -0400

    about

--- src/components/AboutCard.jsx
+++ src/components/AboutCard.jsx
@@ -2,46 +2,50 @@ import React from "react";
 
 const AboutCard = () => {
   return (
-    <section class="text-gray-600 body-font">
-      <div class="container px-5 py-24 mx-auto flex flex-wrap">
-        <div class="lg:w-1/2 w-full mb-10 lg:mb-0 rounded-lg overflow-hidden">
+    <section className="text-gray-600 body-font">
+      <div className="container px-5 py-24 mx-auto flex flex-wrap">
+        <div className="lg:w-1/2 w-full mb-10 lg:mb-0 rounded-lg overflow-hidden">
           <img
             alt="feature"
-            class="object-cover object-center h-full w-full"
-            src="assets/about.jpg"
+            className="object-cover object-center h-full w-full"
+            src="assets/about.jpg" // Ensure the correct path
           />
         </div>
-        <div class="flex flex-col flex-wrap lg:py-6 -mb-10 lg:w-1/2 lg:pl-12 lg:text-left text-center">
-          <div class="flex flex-col mb-10 lg:items-start items-center">
-            <div class="w-12 h-12 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-5">
+        <div className="flex flex-col flex-wrap lg:py-6 -mb-10 lg:w-1/2 lg:pl-12 lg:text-left text-center">
+          {/* Card 1 */}
+          <div className="flex flex-col mb-10 lg:items-start items-center">
+            <div className="w-12 h-12 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-5">
               <svg
                 fill="none"
                 stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                class="w-6 h-6"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                className="w-6 h-6"
                 viewBox="0 0 24 24"
               >
                 <path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
               </svg>
             </div>
-            <div class="flex-grow">
-              <h2 class="text-gray-900 text-lg title-font font-medium mb-3">
+            <div className="flex-grow">
+              <h2 className="text-gray-900 text-lg title-font font-medium mb-3">
                 Our NGO
               </h2>
-              <p class="leading-relaxed text-base">
-                addressing the pressing challenges faced by{" "}
+              <p className="leading-relaxed text-base">
+                Addressing the pressing challenges faced by...
               </p>
-              <a class="mt-3 text-indigo-500 inline-flex items-center">
+              <a
+                href="#"
+                className="mt-3 text-indigo-500 inline-flex items-center"
+              >
                 Learn More
                 <svg
                   fill="none"
                   stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  class="w-4 h-4 ml-2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  className="w-4 h-4 ml-2"
                   viewBox="0 0 24 24"
                 >
                   <path d="M5 12h14M12 5l7 7-7 7"></path>
@@ -49,15 +53,17 @@ const AboutCard = () => {
               </a>
             </div>
           </div>
-          <div class="flex flex-col mb-10 lg:items-start items-center">
-            <div class="w-12 h-12 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-5">
+          
+          {/* Card 2 */}
+          <div className="flex flex-col mb-10 lg:items-start items-center">
+            <div className="w-12 h-12 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-5">
               <svg
                 fill="none"
                 stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                class="w-6 h-6"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                className="w-6 h-6"
                 viewBox="0 0 24 24"
               >
                 <circle cx="6" cy="6" r="3"></circle>
@@ -65,22 +71,25 @@ const AboutCard = () => {
                 <path d="M20 4L8.12 15.88M14.47 14.48L20 20M8.12 8.12L12 12"></path>
               </svg>
             </div>
-            <div class="flex-grow">
-              <h2 class="text-gray-900 text-lg title-font font-medium mb-3">
+            <div className="flex-grow">
+              <h2 className="text-gray-900 text-lg title-font font-medium mb-3">
                 Our NGO
               </h2>
-              <p class="leading-relaxed text-base">
-                addressing the pressing challenges faced by{" "}
+              <p className="leading-relaxed text-base">
+                Addressing the pressing challenges faced by...
               </p>
-              <a class="mt-3 text-indigo-500 inline-flex items-center">
+              <a
+                href="#"
+                className="mt-3 text-indigo-500 inline-flex items-center"
+              >
                 Learn More
                 <svg
                   fill="none"
                   stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  class="w-4 h-4 ml-2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  className="w-4 h-4 ml-2"
                   viewBox="0 0 24 24"
                 >
                   <path d="M5 12h14M12 5l7 7-7 7"></path>
@@ -88,37 +97,42 @@ const AboutCard = () => {
               </a>
             </div>
           </div>
-          <div class="flex flex-col mb-10 lg:items-start items-center">
-            <div class="w-12 h-12 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-5">
+
+          {/* Card 3 */}
+          <div className="flex flex-col mb-10 lg:items-start items-center">
+            <div className="w-12 h-12 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-5">
               <svg
                 fill="none"
                 stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                class="w-6 h-6"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                className="w-6 h-6"
                 viewBox="0 0 24 24"
               >
                 <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2"></path>
                 <circle cx="12" cy="7" r="4"></circle>
               </svg>
             </div>
-            <div class="flex-grow">
-              <h2 class="text-gray-900 text-lg title-font font-medium mb-3">
+            <div className="flex-grow">
+              <h2 className="text-gray-900 text-lg title-font font-medium mb-3">
                 Our NGO
               </h2>
-              <p class="leading-relaxed text-base">
-                addressing the pressing challenges faced by{" "}
+              <p className="leading-relaxed text-base">
+                Addressing the pressing challenges faced by...
               </p>
-              <a class="mt-3 text-indigo-500 inline-flex items-center">
+              <a
+                href="#"
+                className="mt-3 text-indigo-500 inline-flex items-center"
+              >
                 Learn More
                 <svg
                   fill="none"
                   stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  class="w-4 h-4 ml-2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  className="w-4 h-4 ml-2"
                   viewBox="0 0 24 24"
                 >
                   <path d="M5 12h14M12 5l7 7-7 7"></path>
